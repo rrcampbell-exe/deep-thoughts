b157962d700ba8c2d0f55b94909e1a8b1ce52594
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-07 10:35:18 -0500

    Add frontend for thought creation

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import ThoughtList from "../components/ThoughtList";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import FriendList from "../components/FriendList";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
+import { ADD_FRIEND } from "../utils/mutations";
+import ThoughtForm from "../components/ThoughtForm";
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -14,6 +16,19 @@ const Profile = () => {
   });
 
   const user = data?.me || data?.user || {};
+
+  // useMutation and click functionality for adding friends
+  const [addFriend] = useMutation(ADD_FRIEND);
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   // redirect to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -27,7 +42,8 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
@@ -38,6 +54,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -56,6 +78,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
